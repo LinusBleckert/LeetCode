2235b7bf3405002d459abec05c3b578fe28356c0
Linus Bleckert <linusbleckert@pop-os.localdomain>
2021-06-29 21:53:54 +0200

    tried optimnzing sliding windows

--- src/june2021/week5/MaxConsecImprovement.java
+++ src/june2021/week5/MaxConsecImprovement.java
@@ -1,45 +1,25 @@
 package june2021.week5;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class MaxConsecImprovement {
 
     public static int longestOnes(int[] nums, int k){
 
-        int gaps = 0;
-        int[][] indexGaps = new int[nums.length][2];
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                int index = i;
-                int size = 1;
-
-                do{
-                    size++;
-                    i++;
-                }while(i < nums.length && nums[i] == 0);
-                indexGaps[gaps][0] = index;
-                indexGaps[gaps][1] = size;
-                gaps++;
+        int swap = nums[0];
+        int swaps = 1;
+        for(int i=1; i < nums.length; i++){
+            if(swap != nums[i]){
+                swaps++;
+                swap = nums[i];
             }
         }
 
-        int[] tempNums = nums;
-        // indexGapSize should be good now
-        int remainingK = k;
-        for (int i = 0; i < gaps; i++) {
-            for (int j = indexGaps[i][0]; j < indexGaps[i][1]; j++) {
-                tempNums[j] = 1;
-                remainingK--;
-            }
-        }
+        int[][] summary = new int[swaps][2];
 
 
 
-
-        return 0;
+        return swaps;
 
     }
 
@@ -67,8 +47,8 @@ public class MaxConsecImprovement {
 
     public static void main(String[] args) {
 
-        int[] input = {0,0,0,1, 0, 0};
-        System.out.println(longestOnes(input, 4));
+        int[] input = {1,0,1, 0, 0,1,1};
+        System.out.println(longestOnes(input, 3));
 
     }
 }
--- src/june2021/week5/MaxConsecSimpleSlidingWindow.java
+++ src/june2021/week5/MaxConsecSimpleSlidingWindow.java
@@ -1,28 +1,40 @@
 package june2021.week5;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MaxConsecSimpleSlidingWindow {
 
     public static int longestOnes(int[] nums, int k){
 
-        int zeroes = 0;
-        for (int num : nums) {
-            if(num == 0)
-                zeroes++;
-        }
-        if(k > zeroes)
-            k = zeroes;
-        int[] zeroIndex = new int[zeroes];
-        int insertedZeroes = 0;
+
+        List<int[]> ints = new ArrayList<>();
+
+        int indexOfLastOne = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == 0){
-                zeroIndex[insertedZeroes++] = i;
+            if(nums[i] == 1){
+                if((indexOfLastOne - i) > k){
+                    // Split?
+                    ints.add(Arrays.copyOfRange(nums, i, indexOfLastOne));
+                    indexOfLastOne = i;
+                }
             }
         }
 
+        ints.add(Arrays.copyOfRange(nums,indexOfLastOne, nums.length));
+
+
         // Sliding windows now
-        int[] tempNums = nums.clone();
+        int longest = 0;
+        for (int[] anInt : ints) {
+            int current = subRoutine(anInt, k);
+            if(current > longest)
+                longest = current;
+        }
+        return longest;
+        /*int[] tempNums = nums.clone();
         int longest = 0;
         for(int j = 0; j <= (zeroIndex.length - k); j++){
 
@@ -37,12 +49,58 @@ public class MaxConsecSimpleSlidingWindow {
             if(currentLongest > longest)
                 longest = currentLongest;
             tempNums = nums.clone();
+        }*/
+
+        //return longest;
+
+    }
+
+    public static int subRoutine(int nums[], int k){
+        int zeroes = 0;
+        for (int num : nums) {
+            if(num == 0)
+                zeroes++;
         }
 
+        if(k > zeroes)
+            k = zeroes;
+        int[] zeroIndex = new int[zeroes];
+
+        int insertedZeroes = 0;
+        int firstSplit = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0){
+                zeroIndex[insertedZeroes++] = i;
+
+            }
+        }
+
+        // Split into more arrays if zeroIndex[i] - zeroIndex[i-1] > k
 
 
+        // Sliding windows now
+        return findLongestSlidingWindows(nums, zeroIndex, k);
+    }
+
+    public static int findLongestSlidingWindows(int[] original, int[] indexArray, int k){
+
+        int[] tempNums = original.clone();
+        int longest = 0;
+        for(int j = 0; j <= (indexArray.length - k); j++){
+
+            if(j >= original.length)
+                return longest;
+
+            for(int l = j; l < k + j; l++){
+                tempNums[indexArray[l]] = 1;
+            }
+
+            int currentLongest = longOnes(tempNums);
+            if(currentLongest > longest)
+                longest = currentLongest;
+            tempNums = original.clone();
+        }
         return longest;
-
     }
 
     public static int longOnes(int[] nums){
@@ -66,11 +124,10 @@ public class MaxConsecSimpleSlidingWindow {
         return result;
     }
 
-
     public static void main(String[] args) {
 
         int[] input = {1,1,1, 0, 0,0,0};
-        System.out.println(longestOnes(input, 3));
+        System.out.println(longestOnes(input, 1));
 
     }
 }
