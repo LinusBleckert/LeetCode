95cb40a61e3c4c3f0efcd64d1cc7cd9470437e68
Linus Bleckert <linusbleckert@pop-os.localdomain>
2021-06-30 13:48:06 +0200

    Initial LCA

---
+++ src/june2021/week5/LowestCommonAncestor.java
@@ -0,0 +1,50 @@
+package june2021.week5;
+
+public class LowestCommonAncestor {
+
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x){ this.val = x;}
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+
+
+        // 4 cases:
+
+        if(root == null)
+            return null;
+        // found p
+        // needs to be under left and right are null first, since a node can be a descendant to itself
+        if(root == p)
+            return p;
+        if(root == q)
+            return q;
+
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // LCA found
+        if((left == p && right == q) || (right == p && left == q)){
+            return root;
+        }
+        if(left == null && right == null)
+            return null;
+
+        if(left != null)
+            return left;
+        if(right != null)
+            return right;
+
+        return null;
+    }
+
+
+
+}
+
